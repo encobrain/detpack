62d02da943477e13aca69f07dd495f127d4c86be
encobrain <encobrain@gmail.com>
2016-11-17 18:20:19 +0300

    ...

---
+++ tests/01-types.js
@@ -0,0 +1,22 @@
+/**
+ * Created by encobrain on 17.11.16.
+ */
+
+var detpack = require('../index'),
+
+    entities = [
+        ['Uint', 1, [0x81]]
+    ]
+    ;
+
+
+
+module.exports = {
+    'Encode': function (test) {
+
+    },
+
+    'Decode': function (test) {
+
+    }
+};
