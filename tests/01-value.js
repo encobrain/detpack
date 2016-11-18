c84eeba9c68c940bb83688aea680fb5717305bdf
encobrain <encobrain@gmail.com>
2016-11-18 17:01:01 +0300

    Added tests

--- tests/01-value.js
+++ tests/01-value.js
@@ -10,7 +10,58 @@ var detpack = require('../index'),
         ['Uint', 257, [0x01, 0x82]],
         ['Uint', 0x3ffffffffffffe0000, [0x00,0x00,0x78,0x7F,0x7F,0x7F,0x7F,0x7F,0x7F,0xFF]],
         ['Uint', 0x400000000000000000, new Error()],
-        ['Uint', new Error(), [0,0,0,0,0,0,0,0,0,0,0x80]]
+        ['Uint', 'asdfasdf', new Error()],
+        ['Uint', new Error(), [0,0,0,0,0,0,0,0,0,0,0x80]],
+
+        ['Uint8', 1, [0x01]],
+        ['Uint8', 0xFF, [0xFF]],
+        ['Uint8', 0x100, new Error()],
+        ['Uint8', -1, new Error()],
+
+        ['Uint16', 1, [0x01,0x00]],
+        ['Uint16', 255, [0xFF,0x00]],
+        ['Uint16', 256, [0x00,0x01]],
+        ['Uint16', 0xABCD, [0xCD,0xAB]],
+        ['Uint16', 0x10000, new Error()],
+        ['Uint16', -1, new Error()],
+        ['Uint32', 'asdfasdf', new Error()],
+
+        ['Uint32', 1, [0x01,0x00,0x00,0x00]],
+        ['Uint32', 255, [0xFF,0x00,0x00,0x00]],
+        ['Uint32', 0x10000, [0x00,0x00,0x01,0x00]],
+        ['Uint32', 0x01234567, [0x67,0x45,0x23,0x01]],
+        ['Uint32', 0x100000000, new Error()],
+        ['Uint32', -1, new Error()],
+        ['Uint32', 'asdfasdf', new Error()],
+
+        ['Uint64', 1, [0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00]],
+        ['Uint64', 255, [0xFF,0x00,0x00,0x00,0x00,0x00,0x00,0x00]],
+        ['Uint64', 0x100000000, [0x00,0x00,0x00,0x00,0x01,0x00,0x00,0x00]],
+        ['Uint64', 0x0123456789ABCDE0, [0xE0,0xCD,0xAB,0x89,0x67,0x45,0x23,0x01]],
+        ['Uint64', 0x10000000000000000, new Error()],
+        ['Uint64', -1, new Error()],
+        ['Uint64', 'asdfasdf', new Error()],
+
+        ['Int', 1, [0x81]],
+        ['Int', 128, [0x00, 0x81]],
+        ['Int', 255, [0x7F, 0x81]],
+        ['Int',-1, [0xFF]],
+        ['Int',-127, []],
+        ['Int', 0x3ffffffffffffe0000, [0x00,0x00,0x78,0x7F,0x7F,0x7F,0x7F,0x7F,0x7F,0xFF]],
+        ['Int', 0x400000000000000000, new Error()],
+        ['Int', 'asdfasdf', new Error()],
+        ['Int', new Error(), [0,0,0,0,0,0,0,0,0,0,0x80]],
+
+
+        ['Int8', 1, [0x01]],
+        ['Int8', 127, [0x7F]],
+        ['Int8', -1, [0xFF]],
+        ['Int8', -128, [0x80]],
+        ['Int8', 128, new Error()],
+        ['Int8', -129, new Error()],
+        ['Int8', 'asdasdf', new Error()],
+
+
     ]
     ;
 
@@ -33,17 +84,17 @@ module.exports = {
                 encodedBuf = type.encode(value);
 
                 if (buf instanceof Error)
-                    return test.ok(false, typeName + entity[1] +' encode fail');
+                    return test.ok(false, typeName + ' '+ entity[1] +' encode fail');
 
                 buf = new Buffer(buf);
 
-                test.ok(encodedBuf.length === buf.length, typeName + ' encoded length correct');
-                test.deepEqual(encodedBuf, buf, typeName + ' encoded bytes correct');
+                test.ok(encodedBuf.length === buf.length, typeName + ' '+ entity[1] + ' encoded length correct');
+                test.deepEqual(encodedBuf, buf, typeName + ' '+ entity[1] + ' encoded bytes correct');
 
             } catch (err) {
                 if (buf instanceof Error) {
                     test.ok(!buf.message || buf.message === err.code, typeName + entity[1] +' encode fail');
-                } else test.ok(false, typeName + ' encode success');
+                } else test.ok(false, typeName + ' '+ entity[1] + ' encode success');
 
 
             }
